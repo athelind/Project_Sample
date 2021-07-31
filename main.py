ef1f988346604177e3d615033dd79da1a5c7fb71
ite <chow_jian_xian@ite.edu.sg>
2021-07-31 10:51:43 +0800

    Inital

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import pandas as pd
-import os
-#conda install -c anaconda openpyxl
 
-wd = os.getcwd()
-print(os.listdir(wd))
+#TRY TO INSTALL THIS
+import openpyxl
 
 file = "Project_File.xlsx"
 xls = pd.ExcelFile(file)
