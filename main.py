d169fa8f32ccd22a0867df348ac7af6ab0e0e0a6
ite <chow_jian_xian@ite.edu.sg>
2021-07-31 16:26:04 +0800

    Added features: - Charting by Month/Year - Charting by Country - Overall Statistics - Ability to switch between regions and s/no

--- main.py
+++ main.py
@@ -5,24 +5,50 @@ from matplotlib import pyplot as plt
 #TRY TO INSTALL THIS
 import openpyxl
 
+sno = 1
+
+region = "Asia"
+#region = "Europe"
+#region = "Others"
+
+if sno == 1:
+    yearStart = 1978
+elif sno == 2:
+    yearStart = 1988
+elif sno == 3:
+    yearStart = 1998
+elif sno == 4:
+    yearStart = 2008
+
+if region == "Asia":
+    colNo = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    colsNames = ["Year", "Brunei Darussalam", "Indonesia", "Malaysia", "Philippines", "Thailand", "Viet Nam", "Myanmar",
+                 "Japan", "Hong Kong", "China", "Taiwan", "Korea, Republic Of"]
+    countryList = ["Brunei Darussalam", "Indonesia", "Malaysia", "Philippines", "Thailand", "Viet Nam", "Myanmar",
+                   "Japan", "Hong Kong", "China", "Taiwan", "Korea, Republic Of"]
+elif region == "Europe":
+    colNo = [0, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
+    colsNames = ["Year","United Kingdom","Germany","France","Italy","Netherlands","Greece","Belgium & Luxembourg","Switzerland","Austria","Scandinavia","CIS & Eastern Europe"]
+    countryList = ["United Kingdom", "Germany", "France", "Italy", "Netherlands", "Greece",
+                   "Belgium & Luxembourg", "Switzerland", "Austria", "Scandinavia", "CIS & Eastern Europe"]
+elif region == "Others":
+    colNo = [0, 13, 14, 15, 16, 17, 18, 30, 31, 32, 33, 34]
+    colsNames = ["Year", "India", "Pakistan", "Sri Lanka", "Saudi Arabia", "Kuwait", "UAE", "USA", "Canada",
+                 "Australia", "New Zealand", "Africa"]
+    countryList = ["India", "Pakistan", "Sri Lanka", "Saudi Arabia", "Kuwait", "UAE", "USA", "Canada",
+                   "Australia", "New Zealand", "Africa"]
+
+yearEnd = yearStart + 9
 file = "Project_File.xlsx"
 xls = pd.ExcelFile(file)
 
-# Parse the first column of the second sheet and rename the column: df2
-#df2 = xls.parse(1, usecols=[0], skiprows=[0], names=["Country"])
-
-dataFrame = xls.parse(0, usecols=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
-                      names=["Year","Brunei Darussalam","Indonesia","Malaysia","Philippines","Thailand","Viet Nam","Myanmar","Japan","Hong Kong","China","Taiwan","Korea, Republic Of","India","Pakistan","Sri Lanka"])
-#print(dataFrame.head())
-#print(dataFrame.info())
-#print(dataFrame["Year"])
-
+dataFrame = xls.parse(0, usecols=colNo, names=colsNames)
 dataFrame = dataFrame.replace([" na "], [0])
-countryList = ["Brunei Darussalam","Indonesia","Malaysia","Philippines","Thailand","Viet Nam","Myanmar","Japan","Hong Kong","China","Taiwan","Korea, Republic Of","India","Pakistan","Sri Lanka"]
 
-for year in range(2017):
-    if year > 1977 and year < 1988:
-    #if year == 1978:
+######  Charting by Month/Year ######
+
+for year in range(yearStart,  yearEnd + 1):
+    if year >= yearStart and year <= yearEnd:
         targetRows = dataFrame[dataFrame["Year"].str.contains(str(year))]
 
         for country in countryList:
@@ -31,20 +57,69 @@ for year in range(2017):
             else:
                 plt.plot(targetRows["Year"], targetRows[country], label=country)
 
-        print(targetRows)
-
         plt.title("Visitors in " + str(year))
         plt.xlabel("Year")
-        plt.ylabel("Country")
+        plt.ylabel("Visitor No")
         plt.style.use("dark_background")
         plt.legend()
         plt.show()
 
-for jyear in range(2017):
-    if jyear > 1977 and jyear < 1988:
-        # if year == 1978:
+###### Charting by Country ######
 
-        targetRows = dataFrame[dataFrame["Year"].str.contains(str(jyear))]
-        targetRows = targetRows[["Japan"]]
+# dataFrame["Year"] = dataFrame["Year"].str[:5]
+# for country in countryList:
+#     currCountry = dataFrame[["Year", country]]
+#     targetRows = currCountry[(currCountry["Year"].astype(int) >= yearStart) & (currCountry["Year"].astype(int) <= yearEnd)]
+#
+#     plt.plot(targetRows["Year"], targetRows[country], label=country)
+#
+#     plt.title("Visitors from " + country)
+#     plt.xlabel("Year")
+#     plt.ylabel("Visitor No.")
+#     plt.style.use("dark_background")
+#
+#     plt.show()
 
-        print(targetRows)
+###### Overall Statistics ######
+
+overall = xls.parse(0, usecols=colNo,
+                      names=colsNames)
+overall = overall.replace([" na "], [0])
+overall["Year"] = overall["Year"].str[:5]
+targetRows = overall[(overall["Year"].astype(int) >= yearStart) & (overall["Year"].astype(int) <= yearEnd)]
+
+finalTarget = targetRows[countryList]
+
+topThree = []
+topCountry = []
+
+def update_top_three(country, total):
+    if len(topThree) == 3:
+        if total > min(topThree):
+            index = topThree.index(min(topThree))
+            topThree.remove(min(topThree))
+            topThree.append(total)
+            topCountry.remove(topCountry[index])
+            topCountry.append(country)
+    else:
+        topThree.append(total)
+        topCountry.append(country)
+
+for country in countryList:
+    update_top_three(country, finalTarget[country].sum())
+
+print("\nThe top 3 countries for " + str(yearStart) + " - " + str(yearEnd) + " from Asia was:\n")
+
+for index in range(len(topThree)):
+    print(topCountry[index] + " (" + str(topThree[index]) + ") ")
+
+
+
+# dataFrame["Year"] = dataFrame["Year"].str[:5]
+# jpDataFrame = dataFrame[["Year", "Japan"]]
+# targetRows = jpDataFrame[(jpDataFrame["Year"].astype(int) > 1977) & (jpDataFrame["Year"].astype(int) < 1988)]
+# print(targetRows)
+#
+# plt.scatter(targetRows.Year, targetRows.Japan, marker="s", alpha=0.7)
+# plt.show()
+
