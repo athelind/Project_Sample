d54cbf183a380e72c0a66ce3e0a1348e882f8dc3
ite <chow_jian_xian@ite.edu.sg>
2021-07-31 10:50:04 +0800

    Inital

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import pandas as pd
+import os
+#conda install -c anaconda openpyxl
+
+wd = os.getcwd()
+print(os.listdir(wd))
+
+file = "Project_File.xlsx"
+xls = pd.ExcelFile(file)
+print(xls.sheet_names)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-    print("The End")
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
